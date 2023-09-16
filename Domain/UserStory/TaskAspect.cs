3bbbea35b4002979d6004d84c368990a746be8af
Jay Shi <25982097@qq.com>
2023-09-16 22:03:24 +0800

    TaskAspect

---
+++ Domain/UserStory/TaskAspect.cs
@@ -0,0 +1,19 @@
+﻿using Domain.User;
+
+namespace Domain.UserStory
+{
+    public class TaskAspect : ITaskAspect
+    {
+        public TaskReference Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public UserReference Owner { get; set; }
+        public DateTime StartedOn { get; set; }
+        public TimeSpan Period { get; set; }
+        public TaskStatus Status { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public UserReference ModifiedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public UserReference CreatedBy { get; set; }
+    }
+}
