17b1be9cbc0f4cec3849349e25da2409462c2f23
Jay Shi <25982097@qq.com>
2023-09-16 22:09:49 +0800

    UserStoryInformationEntity

--- Infra.Database.MySQL/UserStory/Entities/UserStoryInformationEntity.cs
+++ Infra.Database.MySQL/UserStory/Entities/UserStoryInformationEntity.cs
@@ -6,15 +6,15 @@ namespace Infra.Database.MySQL.UserStory.Entities
     {
         [Key]
         public Guid Id { get; set; }
-        
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public Guid Owner { get; set; }
-
         public DateTime StartedOn { get; set; }
-
         public TimeSpan Period { get; set; }
+        public string Status { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid CreatedBy { get; set; }
     }
 }
