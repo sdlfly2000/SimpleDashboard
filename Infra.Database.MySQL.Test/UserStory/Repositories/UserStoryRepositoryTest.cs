f2c8acddc99bdf0540e181904570f8fa28ad0501
Jay Shi <25982097@qq.com>
2023-10-15 18:02:15 +0800

    System Test for Add

--- Infra.Database.MySQL.Test/UserStory/Repositories/UserStoryRepositoryTest.cs
+++ Infra.Database.MySQL.Test/UserStory/Repositories/UserStoryRepositoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Services.UserStory.Repositories;
+using Domain.User;
+using Domain.UserStory;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleDashboard;
 using SimpleDashboard.Common;
@@ -31,6 +33,37 @@ namespace Infra.Database.MySQL.Test.UserStory.Repositories
 
                 // Assert
                 Assert.IsNotNull(userStoryRepository);
+            }
+        }
+
+        [TestMethod, TestCategory(nameof(TestCategoryEnum.SystemTest))]
+        public void Given_Aspect_When_Add_Then_UserStroyId_Return()
+        {
+            string[] args = Array.Empty<string>();
+
+            // Arrange
+            var host = Program.CreateHostBuilder(args).Build();
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                var userStoryId = Guid.NewGuid();
+                var reference = new UserStoryReference(userStoryId.ToString());
+                var aspect = new UserStoryInformationAspect(reference)
+                {                    
+                    Title = "Title",
+                    Description = "Description",
+                    Owner = new UserReference("6e0ddf4a-638e-11ee-a670-00262da8807c"),
+                    CreatedBy = new UserReference("6e0ddf4a-638e-11ee-a670-00262da8807c"),
+                    ModifiedBy = new UserReference("6e0ddf4a-638e-11ee-a670-00262da8807c")
+                };
+
+                // Act
+                var userStoryRepository = services.GetRequiredService<IUserStoryRepository>();
+                var userStoryIdReturn = userStoryRepository.Add(aspect);
+
+                // Assert
+                Assert.IsNotNull(userStoryIdReturn);
+                Assert.AreEqual(userStoryId, userStoryIdReturn);
             }
         }
     }
