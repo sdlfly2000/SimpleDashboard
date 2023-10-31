c8bf9950645902cf40bb6a527277f3ea78169932
Jay Shi <25982097@qq.com>
2023-10-31 22:02:54 +0800

    add presentation layer

---
+++ Presentation.WebApi/JWTOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.WebApi
+{
+    public class JWTOptions
+    {
+        public string SigningKey { get; set; }
+        public int ExpireSeconds { get; set; }
+    }
+}
---
+++ Presentation.WebApi/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Presentation.WebApi;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<JWTOptions>(builder.Configuration.GetSection("JWT"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option =>
+                {
+                    var jwtOpt = builder.Configuration.GetSection("JWT").Get<JWTOptions>();
+                    byte[] keyBytes = Encoding.UTF8.GetBytes(jwtOpt.SigningKey);
+                    var secKey = new SymmetricSecurityKey(keyBytes);
+                    option.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = secKey
+                    };
+                });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
