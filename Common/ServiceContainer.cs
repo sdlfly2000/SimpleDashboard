9712fb6d5107b5d4a426737509bb83d5a0faa76a
Jay Shi <25982097@qq.com>
2023-11-18 14:56:53 +0800

    Add ServiceContainer For Unit Test

---
+++ Common/ServiceContainer.cs
@@ -0,0 +1,15 @@
+﻿using Common.Core.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleDashboard.Common
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection CreateDefault()
+        {
+            var container = new ServiceCollection();
+            container.RegisterDomain("Application.Services", "Domain.Services", "Infra.Database.MySQL");
+            return container;
+        }
+    }
+}
