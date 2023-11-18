432c8ecea8fe1bef1753c8e69b3592ef36f5b223
Jay Shi <25982097@qq.com>
2023-11-18 15:16:35 +0800

    fix DI unit test

---
+++ Infra.Database.MySQL.Test/UserStory/Repositories/TaskRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Services.UserStory.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleDashboard.Common;
+using SimpleDashboard.Extentions;
+
+namespace Infra.Database.MySQL.Test.UserStory.Repositories
+{
+    [TestClass]
+    public class TaskRepositoryTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod, TestCategory(nameof(TestCategoryEnum.UnitTest))]
+        public void Given_UserStoryId_When_LoadTask_Then_TaskAspect_Return()
+        {
+            string[] args = Array.Empty<string>();
+
+            // Arrange
+            var serviceContainer = ServiceContainer.CreateDefaultForUnitTest();
+            using (var serviceScope = serviceContainer.BuildServiceProvider())
+            {
+                // Act
+                var taskRepository = serviceScope.GetRequiredService<ITaskRepository>();
+
+                // Assert
+                Assert.IsNotNull(taskRepository);
+            }
+        }
+    }
+}
--- SimpleDashboard.Extentions/ServiceContainer.cs
+++ SimpleDashboard.Extentions/ServiceContainer.cs
@@ -1,13 +1,16 @@
 ﻿using Common.Core.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace SimpleDashboard.Common
+namespace SimpleDashboard.Extentions
 {
     public static class ServiceContainer
     {
-        public static IServiceCollection CreateDefault()
+        public static IServiceCollection CreateDefaultForUnitTest()
         {
             var container = new ServiceCollection();
+
+            container.AddMemoryCache();
+            container.AddMySQLDatabase("server=192.168.71.151;database=SimpleDashboard;uid=sdlfly2000;password=sdl@1215;");
             container.RegisterDomain("Application.Services", "Domain.Services", "Infra.Database.MySQL");
             return container;
         }
