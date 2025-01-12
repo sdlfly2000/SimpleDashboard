438a36b4db6351d13fc12955649033cb212d8a96
Jay Shi <25982097@qq.com>
2025-01-12 22:08:49 +0800

    Add Primeng settings

--- Presentation.Web.Angular/src/app/app.module.ts
+++ Presentation.Web.Angular/src/app/app.module.ts
@@ -12,20 +12,36 @@ import { MainComponent } from './main/main.component';
 import { QueryStringService } from '../services/shared.QueryString.service';
 import { AuthService } from '../services/auth.service';
 
-@NgModule({ declarations: [
-        AppComponent,
-        NavMenuComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', component: MainComponent, pathMatch: 'full' },
-        ])], providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: AuthFailureInterceptor, multi: true },
-        { provide: "BASE_URL", useValue: document.getElementsByTagName('base')[0].href },
-        { provide: AuthService },
-        { provide: QueryStringService },
-        provideHttpClient(withInterceptorsFromDi())
-    ] })
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+        { path: '', component: MainComponent, pathMatch: 'full' },
+    ])
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthFailureInterceptor, multi: true },
+    { provide: "BASE_URL", useValue: document.getElementsByTagName('base')[0].href },
+    { provide: AuthService },
+    { provide: QueryStringService },
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
+})
 export class AppModule { }
