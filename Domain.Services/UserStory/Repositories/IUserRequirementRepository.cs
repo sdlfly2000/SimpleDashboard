6484e3bf15a6d3b9273f725a0e5b37edc86bd5d0
Jay Shi <25982097@qq.com>
2025-02-04 15:55:37 +0800

    Moved sql script to dedicated folder

---
+++ Domain.Services/UserStory/Repositories/IUserRequirementRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.UserRequirement;
+
+namespace Domain.Services.UserStory.Repositories
+{
+    public interface IUserRequirementRepository : IRepository<long, UserRequirementEntity>
+    {
+        Task<IList<UserRequirementEntity>> LoadByOwnerId(Guid id);
+    }
+}
