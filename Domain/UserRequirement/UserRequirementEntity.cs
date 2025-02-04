6118c52d15b18dfbd047f95efeccf9a0fc775d3e
Jay Shi <25982097@qq.com>
2025-02-04 13:13:40 +0800

    Add UserRequirement

---
+++ Domain/UserRequirement/UserRequirementEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.UserRequirement
+{
+    public class UserRequirementEntity : BaseRecord
+    {
+        public UserRequirementEntity()
+        {
+            UserStories = new List<UserStoryEntity>();
+        }
+
+        public UserRequirementEntity(UserRequirementReference reference)
+        {
+            Reference = reference;
+            UserStories = new List<UserStoryEntity>();
+        }
+
+        public List<UserStoryEntity> UserStories { get; set; }
+    }
+}
---
+++ Domain/UserRequirement/UserRequirementReference.cs
@@ -0,0 +1,11 @@
+﻿using SimpleDashboard.Common;
+
+namespace Domain.UserRequirement
+{
+    public class UserRequirementReference : Reference
+    {
+        public UserRequirementReference(long code) : base(code.ToString(), CacheField.UserRequirement)
+        {
+        }
+    }
+}
