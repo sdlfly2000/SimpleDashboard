81745a169d56a3a0f3860ff7dbbbf5da5e7e3b1f
Jay Shi <25982097@qq.com>
2025-02-16 18:26:50 +0800

    Improve UserReference

--- Domain/User/UserReference.cs
+++ Domain/User/UserReference.cs
@@ -4,18 +4,35 @@ namespace Domain.User
 {
     public class UserReference : Reference
     {
-        public UserReference(string code) : base(code, CacheField.User)
+        public UserReference(Guid code) : base(code.ToString(), CacheField.User)
         {
         }
 
-        public static UserReference Parse(string code)
+        public static UserReference Parse(Guid code)
         {
-            if (string.IsNullOrEmpty(code))
+            if (code == null)
             {
                 return default;
             }
 
             return new UserReference(code);
+        }
+
+        public static explicit operator UserReference(Guid code)
+        {
+            return Parse(code);
+        }
+
+        public static explicit operator UserReference(string code)
+        {
+            if(Guid.TryParse(code, out var guidCode))
+            {
+                return Parse(guidCode);
+            }
+            else
+            {
+                return default;
+            }
         }
     }
 }
